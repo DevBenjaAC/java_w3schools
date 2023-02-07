26c8376dad1afb5b9d1d1fabde4d93789d47d0aa
DevBenjaAC-BtoB <benjamin.alvarado@btob.com.mx>
2023-02-07 12:19:33 -0600

    feat: add new notes of matrices

---
+++ java-projects/MyNewProject.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mynewproject;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Developer
+ */
+public class MyNewProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, i'm fine mr.Stark!");
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        for (int i = 0; i < myNumbers.length; ++i) {
+        for(int j = 0; j < myNumbers[i].length; ++j) {
+        System.out.println(myNumbers[i][j]);
+        System.out.println("Hello, Good morning mr.Stark!");
+      }
+    }
+    }
+}
